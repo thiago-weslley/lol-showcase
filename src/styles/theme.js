2555159aac3356dfd14bdc69fe7a71d6ee5049f4
thiago-weslley <thiagoweslley@yahoo.com>
2023-10-06 16:51:44 -0300

    feat: Create theme file 🎨

---
+++ src/styles/theme.js
@@ -0,0 +1,18 @@
+export const theme = {
+  colors: {
+    //Primary Colors
+    yellow_600: "#f4c258",
+    yellow_700: "#c89b3c",
+
+    //Gray Colors
+    gray_50: "#f9fafb",
+    gray_100: "#f3f4f6",
+    gray_200: "#e5e7eb",
+    gray_400: "#9ca3af",
+    gray_600: "#4b5563",
+
+    //Neutral Colors
+    white: "#ffffff",
+    black: "#020912",
+  },
+};

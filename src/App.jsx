e78fd60f5e0f37c78230408283c800bc8c5f341a
thiago-weslley <thiagoweslley@yahoo.com>
2023-10-09 12:14:17 -0300

    feat: Creates and apply `globalStyles`

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,17 @@
 import { BrowserRouter } from "react-router-dom";
 import { Router } from "./routes/router";
 
+import { ThemeProvider } from "styled-components";
+import { theme } from "./styles/theme";
+import { GlobalStyles } from "./styles/globalStyles";
+
 export function App() {
   return (
-    <BrowserRouter>
-      <Router />
-    </BrowserRouter>
+    <ThemeProvider theme={theme}>
+      <BrowserRouter>
+        <Router />
+        <GlobalStyles />
+      </BrowserRouter>
+    </ThemeProvider>
   );
 }
---
+++ src/styles/globalStyles.js
@@ -0,0 +1,21 @@
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyles = createGlobalStyle`
+  :root {
+    font-size: 62.5%;
+  }
+
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    text-decoration: none;
+    -webkit-tap-highlight-color: transparent;
+    -webkit-font-smoothing: antialiased;
+  }
+
+  body {
+    background: ${({ theme }) => theme.colors.black};
+    font-size: 1.6rem;
+  }
+`;
